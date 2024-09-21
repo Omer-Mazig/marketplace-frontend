8356bc3541bd1886bf5be60d16a0e00734fb31ee
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-22 00:01:48 +0300

    added condition for user profile (no product and wishlist)

--- src/pages/user-profile/user-products/user-products-page.tsx
+++ src/pages/user-profile/user-products/user-products-page.tsx
@@ -28,38 +28,42 @@ function UserProductsPage() {
         <CardDescription>Manage the products you've listed.</CardDescription>
       </CardHeader>
       <CardContent>
-        <ul className="space-y-4">
-          {userProfileData.products.map((product) => {
-            return (
-              <li
-                key={product.id}
-                className="text-center 3xs:text-start flex flex-col md:flex-row justify-between 3xs:items-start md:items-center border-b pb-2"
-              >
-                <span className="mb-2 md:mb-0">{product.name}</span>
-                <div className="flex flex-col 3xs:flex-row justify-center flex-wrap gap-2">
-                  <Badge
-                    variant="outline"
-                    className="justify-center"
-                  >
-                    ${product.price?.toFixed(2)}
-                  </Badge>
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                  >
-                    Delete
-                  </Button>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        {userProfileData.products.length ? (
+          <ul className="space-y-4">
+            {userProfileData.products.map((product) => {
+              return (
+                <li
+                  key={product.id}
+                  className="text-center 3xs:text-start flex flex-col md:flex-row justify-between 3xs:items-start md:items-center border-b pb-2"
+                >
+                  <span className="mb-2 md:mb-0">{product.name}</span>
+                  <div className="flex flex-col 3xs:flex-row justify-center flex-wrap gap-2">
+                    <Badge
+                      variant="outline"
+                      className="justify-center"
+                    >
+                      ${product.price?.toFixed(2)}
+                    </Badge>
+                    <Button
+                      variant="ghost"
+                      size="sm"
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      size="sm"
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <p>You have no products</p>
+        )}
       </CardContent>
       <CardFooter>
         <Button className="w-full md:w-auto">Add New Product</Button>
--- src/pages/user-profile/user-wishlist/user-wishlist-page.tsx
+++ src/pages/user-profile/user-wishlist/user-wishlist-page.tsx
@@ -27,25 +27,29 @@ function UserWishlistPage() {
         <CardDescription>Products you're interested in.</CardDescription>
       </CardHeader>
       <CardContent>
-        <ul className="space-y-4">
-          {userProfileData.wishlist.map((item) => (
-            <li
-              key={item.id}
-              className="flex flex-col md:flex-row justify-between items-start md:items-center border-b pb-2"
-            >
-              <span className="mb-2 md:mb-0">{item.name}</span>
-              <div className="flex flex-wrap gap-2">
-                <Badge variant="secondary">${item.price.toFixed(2)}</Badge>
-                <Button
-                  variant="ghost"
-                  size="sm"
-                >
-                  Remove
-                </Button>
-              </div>
-            </li>
-          ))}
-        </ul>
+        {userProfileData.wishlist.length ? (
+          <ul className="space-y-4">
+            {userProfileData.wishlist.map((item) => (
+              <li
+                key={item.id}
+                className="flex flex-col md:flex-row justify-between items-start md:items-center border-b pb-2"
+              >
+                <span className="mb-2 md:mb-0">{item.name}</span>
+                <div className="flex flex-wrap gap-2">
+                  <Badge variant="secondary">${item.price.toFixed(2)}</Badge>
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                  >
+                    Remove
+                  </Button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>You have no saved products </p>
+        )}
       </CardContent>
     </Card>
   );
