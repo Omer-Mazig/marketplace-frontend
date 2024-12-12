43a6234b0051499d148ee96f0151452d42e07af6
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-12-12 14:52:10 +0200

    modify sidebar

--- src/components/custom/user-button.tsx
+++ src/components/custom/user-button.tsx
@@ -1,67 +1,138 @@
-// Third-party libraries
-import { NavLink } from "react-router-dom";
-import { LogOut } from "lucide-react";
+"use client";
 
-// UI components
-import { Button } from "@/components/ui/button";
+import {
+  BadgeCheck,
+  Bell,
+  ChevronsUpDown,
+  CreditCard,
+  LogOut,
+  LucideIcon,
+  Sparkles,
+} from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
-// Custom providers
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
 import { useAuth } from "@/providers/auth-provider";
 
 export function UserButton() {
+  const { isMobile } = useSidebar();
   const { loggedInUser, logout } = useAuth();
 
+  if (!loggedInUser) return null;
+
+  const avatarFallbackText =
+    loggedInUser.firstName[0].toUpperCase() +
+    loggedInUser.lastName[0].toUpperCase();
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="rounded-full"
-        >
-          <Avatar>
-            <AvatarImage
-              src={loggedInUser?.imageUrl || ""}
-              alt={loggedInUser?.email}
-            />
-            <AvatarFallback>
-              {loggedInUser?.email[0].toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Profile Menu</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <NavLink to="user-profile/info">Profile</NavLink>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <NavLink to="user-profile/settings">Setting</NavLink>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <NavLink to="user-profile/products">Products</NavLink>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <NavLink to="user-profile/wishlist">Wishlist</NavLink>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          onClick={logout}
-          className="flex items-center gap-1"
-        >
-          <LogOut className="h-4 w-4" /> Logout
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage
+                  src={loggedInUser.imageUrl}
+                  alt={loggedInUser.firstName}
+                />
+                <AvatarFallback className="rounded-lg">
+                  {avatarFallbackText}
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">
+                  {loggedInUser.firstName}
+                </span>
+                <span className="truncate text-xs">{loggedInUser.email}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage
+                    src={loggedInUser.imageUrl}
+                    alt={loggedInUser.firstName}
+                  />
+                  <AvatarFallback className="rounded-lg">
+                    {avatarFallbackText}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">
+                    {loggedInUser.firstName}
+                  </span>
+                  <span className="truncate text-xs">{loggedInUser.email}</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="flex gap-2">
+                <Sparkles className="w-4 h-4" />
+                Upgrade to Pro
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="flex gap-2">
+                <BadgeCheck className="w-4 h-4" />
+                Account
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex gap-2">
+                <CreditCard className="w-4 h-4" />
+                Billing
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex gap-2">
+                <Bell className="w-4 h-4" />
+                Notifications
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={logout}
+              className="flex gap-2"
+            >
+              <LogOut className="w-4 h-4" />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
+
+function MenuItem({ label, icon }: { label: string; icon: LucideIcon }) {
+  const Icon = icon;
+  return (
+    <DropdownMenuItem>
+      <Icon />
+      {label}
+    </DropdownMenuItem>
   );
 }
