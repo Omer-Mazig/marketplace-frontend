e5b283d64894b437d6750e85340dcd771fde3008
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-12-14 19:26:28 +0200

    Update user-button.tsx

--- src/components/shared/user-button.tsx
+++ src/components/shared/user-button.tsx
@@ -49,6 +49,7 @@ export function UserButton() {
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
+              tooltip="Account"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
