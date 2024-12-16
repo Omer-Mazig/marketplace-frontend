ffa3bca67b20a86ad01381f8ba73a5e7e10c832b
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-12-16 23:59:18 +0200

    moved themeProvider down the tree

--- src/layouts/platform-layout.tsx
+++ src/layouts/platform-layout.tsx
@@ -9,21 +9,26 @@ import { AppHeader } from "@/components/app/app-header/app-header";
 import { AppFooter } from "@/components/app/app-footer/app-footer";
 import { UpgradePlanDialog } from "@/components/shared/upgrade-plan-dialog";
 
+// Providers
+import { ThemeProvider } from "@/providers/theme-provider";
+
 export default function PlatformLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <SidebarProvider>
-        <AppSidebar />
-        <div className="flex flex-col max-w-screen-2xl flex-grow">
-          <AppHeader />
-          <main className="container flex-grow">
-            <Outlet />
-          </main>
-          <AppFooter />
-        </div>
-      </SidebarProvider>
-      <LoginAlertDialog />
-      <UpgradePlanDialog />
-    </div>
+    <ThemeProvider>
+      <div className="flex flex-col min-h-screen">
+        <SidebarProvider>
+          <AppSidebar />
+          <div className="flex flex-col max-w-screen-2xl flex-grow">
+            <AppHeader />
+            <main className="container flex-grow">
+              <Outlet />
+            </main>
+            <AppFooter />
+          </div>
+        </SidebarProvider>
+        <LoginAlertDialog />
+        <UpgradePlanDialog />
+      </div>
+    </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Custom providers
 import { AuthProvider } from "@/providers/auth-provider.tsx";
-import { ThemeProvider } from "@/providers/theme-provider.tsx";
 import { UpgradePlanDialogProvider } from "./providers/upgrade-plan-dialog-provider.tsx";
 
 // Custom components
@@ -25,16 +24,14 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <AuthProvider>
-          <UpgradePlanDialogProvider>
-            <BreadcrumpProvider>
-              <App />
-            </BreadcrumpProvider>
-          </UpgradePlanDialogProvider>
-          <Toaster />
-        </AuthProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <UpgradePlanDialogProvider>
+          <BreadcrumpProvider>
+            <App />
+          </BreadcrumpProvider>
+        </UpgradePlanDialogProvider>
+        <Toaster />
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
