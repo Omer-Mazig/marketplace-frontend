95fef98cbf5f8a6994960025c504b84bc89a3b40
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-12-16 17:26:53 +0200

    Update new-product-page.tsx

--- src/pages/products/new-product/new-product-page.tsx
+++ src/pages/products/new-product/new-product-page.tsx
@@ -1,10 +1,13 @@
 // Third-party libraries
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Custom components
 import { AfterCreateProductDialog } from "@/pages/products/_components/after-create-product-dialog";
 import { PageHeading } from "@/components/ui/page-heading";
 import { NewProductForm } from "./_components/new-product-form";
+
+// Providers
+import { useSetBreadcrumpItems } from "@/providers/breadcrump-provider";
 
 // TODO: add skelaton
 export default function NewProductPage() {
@@ -12,6 +15,16 @@ export default function NewProductPage() {
     shouldShowafterCreateProductDialog,
     setShouldShowAfterCreateProductDialog,
   ] = useState(false);
+
+  const setBreadcrumpItems = useSetBreadcrumpItems();
+
+  useEffect(() => {
+    setBreadcrumpItems([
+      { href: "/", label: "Home" },
+      { href: "/products", label: "Products" },
+      { href: "/products/new-product", label: "New Product" },
+    ]);
+  }, []);
 
   return (
     <div className="text-center 3xs:text-start">
