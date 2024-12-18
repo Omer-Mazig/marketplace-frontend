784da4d310509733429cf7d4802ae7639076bb42
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-12-18 10:06:02 +0200

    stop propegation for product previewe

--- src/pages/products/_components/edit-product-button.tsx
+++ src/pages/products/_components/edit-product-button.tsx
@@ -17,6 +17,7 @@ export function EditProductButton() {
             asChild
             variant="ghost"
             size="sm"
+            onClick={(e) => e.stopPropagation()}
           >
             <Link to="/platform/products/edit-product">
               <Pencil className="w-5 h-5 text-gray-400" />
